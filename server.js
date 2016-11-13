5957496a7d235e2e43dca837c5f00b1dd4884f9c
Stefan <stefantrajkovic777@gmail.com>
2016-11-13 23:15:38 +0100

    client site created, angular and bootstrap(installed with bower)

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var app = express();
 // View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
